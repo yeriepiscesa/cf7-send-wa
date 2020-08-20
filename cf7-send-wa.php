d763445b7523b473322f47a029ab2c7ca0f20ead
yerie <yeriepiscesa@yahoo.com>
2020-08-20 18:01:22 +0700

    Bump version to 0.11.3

--- cf7-send-wa.php
+++ cf7-send-wa.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 0.3.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'CF7_SEND_WA_VERSION', '0.11.2' );
+define( 'CF7_SEND_WA_VERSION', '0.11.3' );
 
 /**
  * The code that runs during plugin activation.

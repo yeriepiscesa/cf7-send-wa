d77ea228aff85357be0b35d1e13901953950698e
yerie <yeriepiscesa@yahoo.com>
2020-11-17 09:49:10 +0700

    fix hook to display single product WA popup form

--- includes/class-cf7-send-wa.php
+++ includes/class-cf7-send-wa.php
@@ -244,8 +244,10 @@ class Cf7_Send_Wa {
 			
 			$this->loader->add_action( 'wp_ajax_cf7sendwa_products', $plugin_public, 'web_list_product' );
 			$this->loader->add_action( 'wp_ajax_nopriv_cf7sendwa_products', $plugin_public, 'web_list_product' );
-			
-			$this->loader->add_action( 'woocommerce_single_product_summary', $plugin_public, 'cf7_wa_button', 90 );
+
+			$_types = array( 'simple', 'variable', 'external' );
+			foreach( $_types as $t )			
+				$this->loader->add_action( 'woocommerce_'.$t.'_add_to_cart', $plugin_public, 'cf7_wa_button', 90 );
 
 			$this->loader->add_action( 'wp_ajax_cf7sendwa_add_to_cart', $plugin_public, 'quickshop_add_to_cart' );
 			$this->loader->add_action( 'wp_ajax_nopriv_cf7sendwa_add_to_cart', $plugin_public, 'quickshop_add_to_cart' );
